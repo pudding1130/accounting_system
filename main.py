394b38c82cfa37161af7863a232c28e07677b197
eggie <pudding1130s@gmail.com>
2024-07-04 00:19:41 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
 from user import Users
 from process import process
 if __name__ == '__main__' :
+    globals()['user_now'] = "未登入"
     flag = 0
     while flag == 0:
+        print("==================================================================")
+        print(f"目前身份為:{globals()['user_now']}")
+        print("==================================================================")
         flag = process(flag)
+        
